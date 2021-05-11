029fb67486fb264bc59220ab98eb9fd7a96f62f6
Edhrendal <devrendal@protonmail.com>
2021-05-11 23:31:16 +0200

    ✨ Doctrine
    * ➕ `Doctrine`
    * 📦️ Ajout des extensions PHP pour la bdd
    * ⬆️`maker`
    
    -----
    
    * 🗃️ Ajout d'une première entité test

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,6 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
---
+++ src/Entity/Equipment.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EquipmentRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\SerializedName;
+
+/**
+ * @ORM\Entity(repositoryClass=EquipmentRepository::class)
+ */
+final class Equipment extends Item
+{
+    /**
+     * @ORM\Column(length=20, nullable=true)
+     * @SerializedName("manufacturing_license")
+     */
+    protected ?string $manufacturingLicense = null;
+
+    /**
+     * @ORM\Column(length=20, nullable=true)
+     * @SerializedName("enhancement_license")
+     */
+    protected ?string $enhancementLicense = null;
+
+    public function getManufacturingLicense(): ?string
+    {
+        return $this->manufacturingLicense;
+    }
+
+    /** @return $this */
+    public function setManufacturingLicense(?string $manufacturingLicense): self
+    {
+        $this->manufacturingLicense = $manufacturingLicense;
+
+        return $this;
+    }
+
+    public function getEnhancementLicense(): ?string
+    {
+        return $this->enhancementLicense;
+    }
+
+    /** @return $this */
+    public function setEnhancementLicense(?string $enhancementLicense): self
+    {
+        $this->enhancementLicense = $enhancementLicense;
+
+        return $this;
+    }
+}
