5e8c65373e6d3cb9a926ab37661767c9686f06b3
Edhrendal <devrendal@protonmail.com>
2021-05-16 12:08:43 +0200

    🍱 Choix multiples sélectionnables
    * Modification des choix multiples avec des `checkboxes` pour les sélectionner

--- assets/components/form/SelectField.js
+++ assets/components/form/SelectField.js
@@ -1,7 +1,10 @@
 import React from "react";
+import Checkbox from '@material-ui/core/Checkbox';
 import FormControl from "@material-ui/core/FormControl";
 import FormHelperText from "@material-ui/core/FormHelperText";
 import InputLabel from "@material-ui/core/InputLabel";
+import ListItemText from '@material-ui/core/ListItemText';
+import MenuItem from '@material-ui/core/MenuItem';
 import Select from "@material-ui/core/Select";
 
 export default function SelectField(props) {
@@ -11,16 +14,7 @@ export default function SelectField(props) {
         setSelectValue(event.target.value);
     };
 
-    const handleChangeMultiple = (event) => {
-        const { options } = event.target;
-        const value = [];
-        for (let i = 0, l = options.length; i < l; i += 1) {
-            if (options[i].selected) {
-                value.push(options[i].value);
-            }
-        }
-        setSelectValue(value);
-    };
+    const displaySelected = (selected) => selected.map((value) => props.widget.choices[value]).join(", ");
 
     return (
         <FormControl
@@ -43,15 +37,21 @@ export default function SelectField(props) {
                     name: props.widget.name,
                 } }
                 multiple={ props.widget.multiple }
-                native
-                onChange={ props.widget.multiple ? handleChangeMultiple : handleChange }
+                native={ props.widget.multiple === false }
+                renderValue={ displaySelected }
+                onChange={ handleChange }
                 value={ selectValue }
             >
                 {
                     Object.keys(props.widget.choices).map((value) => (
-                        <option key={value} value={value}>
-                            {props.widget.choices[value]}
-                        </option>
+                        props.widget.multiple
+                            ? <MenuItem key={value} value={value}>
+                                <Checkbox checked={selectValue.indexOf(value) > -1} />
+                                <ListItemText primary={props.widget.choices[value]} />
+                            </MenuItem>
+                            : <option key={value} value={value}>
+                                {props.widget.choices[value]}
+                            </option>
                     ))
                 }
             </Select>
