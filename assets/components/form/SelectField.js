47b5d4af31e124997abecd076960d9eb0c4019ab
Edhrendal <devrendal@protonmail.com>
2021-05-17 22:56:15 +0200

    🚑️ Choix multiples sélectionnables
    * Fix temporaire pour sélectionner plusieurs choix d'un "multiple"

--- assets/components/form/SelectField.js
+++ assets/components/form/SelectField.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Checkbox from '@material-ui/core/Checkbox';
+// import Checkbox from '@material-ui/core/Checkbox';
 import FormControl from "@material-ui/core/FormControl";
 import FormHelperText from "@material-ui/core/FormHelperText";
 import InputLabel from "@material-ui/core/InputLabel";
-import ListItemText from '@material-ui/core/ListItemText';
-import MenuItem from '@material-ui/core/MenuItem';
+// import ListItemText from '@material-ui/core/ListItemText';
+// import MenuItem from '@material-ui/core/MenuItem';
 import Select from "@material-ui/core/Select";
 
 export default function SelectField(props) {
@@ -14,7 +14,18 @@ export default function SelectField(props) {
         setSelectValue(event.target.value);
     };
 
-    const displaySelected = (selected) => selected.map((value) => props.widget.choices[value]).join(", ");
+    const handleChangeMultiple = (event) => {
+        const { options } = event.target;
+        const value = [];
+        for (let i = 0, l = options.length; i < l; i += 1) {
+            if (options[i].selected) {
+                value.push(options[i].value);
+            }
+        }
+        setSelectValue(value);
+    };
+
+    // const displaySelected = (selected) => selected.map((value) => props.widget.choices[value]).join(", ");
 
     return (
         <FormControl
@@ -37,21 +48,29 @@ export default function SelectField(props) {
                     name: props.widget.name,
                 } }
                 multiple={ props.widget.multiple }
-                native={ props.widget.multiple === false }
-                renderValue={ displaySelected }
-                onChange={ handleChange }
+                native
+                onChange={ props.widget.multiple ? handleChangeMultiple : handleChange }
                 value={ selectValue }
+                // native={ props.widget.multiple === false }
+                // renderValue={ displaySelected }
+                // onChange={ handleChange }
+                // value={ selectValue }
             >
                 {
+                    // Object.keys(props.widget.choices).map((value) => (
+                    //     props.widget.multiple
+                    //         ? <MenuItem key={value} value={value}>
+                    //             <Checkbox checked={selectValue.indexOf(value) > -1} />
+                    //             <ListItemText primary={props.widget.choices[value]} />
+                    //         </MenuItem>
+                    //         : <option key={value} value={value}>
+                    //             {props.widget.choices[value]}
+                    //         </option>
+                    // ))
                     Object.keys(props.widget.choices).map((value) => (
-                        props.widget.multiple
-                            ? <MenuItem key={value} value={value}>
-                                <Checkbox checked={selectValue.indexOf(value) > -1} />
-                                <ListItemText primary={props.widget.choices[value]} />
-                            </MenuItem>
-                            : <option key={value} value={value}>
-                                {props.widget.choices[value]}
-                            </option>
+                        <option key={value} value={value}>
+                            {props.widget.choices[value]}
+                        </option>
                     ))
                 }
             </Select>
